7fd78317e3c883cfc039e5d7a47d1cf8cbffec07
Trần Trọng Nguyên <trantrongnguyen2552003@gmail.com>
2024-03-11 07:09:43 +0700

    typo: rename HorizalNavigationBar to HorizontalNavigationBar

--- src/App.js
+++ src/App.js
@@ -4,18 +4,14 @@ import VerticalSideBar from "./views/components/VerticalSideBar/VerticalSideBar.
 import HomeScreen from "./views/screens/HomeScreen/HomeScreen.js"
 import PolicyScreen from "./views/screens/PolicyScreen/PolicyScreen.js"
 import PostScreen from "./views/screens/PostScreen/PostScreen.js"
-import HorizontalNavigationBar from "./views/components/HorizalNavigationBar/HorizontalNavigationBar.js"
+import HorizontalNavigationBar from "./views/components/HorizontalNavigationBar/HorizontalNavigationBar.js"
 import { useState } from "react"
+
 function App() {
     let [screenWidth, setScreenWidth] = useState(window.innerWidth)
-    window.onresize = () => {
-        setScreenWidth(window.innerWidth)
-        console.log(typeof screenWidth)
-        console.log(screenWidth)
-    }
+    window.onresize = () => setScreenWidth(window.innerWidth)
 
     let [sectionStates, setSectionStates] = useState([true, false, false])
-
     function changeSectionStates(section) {
         let tempSectionStates = [false, false, false]
 
